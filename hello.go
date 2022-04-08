1afcb1cc641824893fdd2c6016121da33984decb
Subham Kumar <subhamk@juniper.net>
2022-04-08 10:31:13 +0530

    crud operations in golang

---
+++ hello.go
@@ -0,0 +1,216 @@
+package main
+
+// import packages
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+const (
+	HOST        = "localhost"
+	PORT        = 5432
+	DB_USER     = "postgres"
+	DB_PASSWORD = "postgres"
+	DB_NAME     = "movies"
+)
+
+// DB set up
+func setupDB() *sql.DB {
+
+	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", HOST, PORT, DB_USER, DB_PASSWORD, DB_NAME)
+	db, err := sql.Open("postgres", psqlconn)
+
+	checkErr(err)
+	return db
+
+}
+
+type Movie struct {
+	MovieID   string `json:"movieid"`
+	MovieName string `json:"moviename"`
+}
+
+type JsonResponse struct {
+	Type    string  `json:"type"`
+	Data    []Movie `json:"data"`
+	Message string  `json:"message"`
+}
+
+// Function for handling messages
+func printMessage(message string) {
+	fmt.Println("")
+	fmt.Println(message)
+	fmt.Println("")
+}
+
+// Function for handling errors
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+// Get all movies
+
+// response and request handlers
+func GetMovies(w http.ResponseWriter, r *http.Request) {
+	db := setupDB()
+
+	printMessage("Getting movies...")
+
+	// Get all movies from movies table that don't have movieID = "1"
+	rows, err := db.Query("SELECT * FROM movies")
+
+	// check errors
+	checkErr(err)
+
+	// var response []JsonResponse
+	var movies []Movie
+
+	// Foreach movie
+	for rows.Next() {
+		var id int
+		var movieID string
+		var movieName string
+
+		err = rows.Scan(&id, &movieID, &movieName)
+
+		// check errors
+		checkErr(err)
+		db.Close()
+		movies = append(movies, Movie{MovieID: movieID, MovieName: movieName})
+	}
+
+	var response = JsonResponse{Type: "success", Data: movies}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+// Create a movie
+
+// response and request handlers
+func CreateMovie(w http.ResponseWriter, r *http.Request) {
+
+	// unMarshalling the json body of postman to get the attributes.
+	body, _ := ioutil.ReadAll(r.Body) // check for errors
+
+	keyVal := make(map[string]string)
+	json.Unmarshal(body, &keyVal) // check for errors
+
+	movieID := keyVal["movieid"]
+	movieName := keyVal["moviename"]
+
+	fmt.Println(movieID + " " + movieName)
+
+	var response = JsonResponse{}
+
+	if movieID == "" || movieName == "" {
+		response = JsonResponse{Type: "error", Message: "You are missing movieID or movieName parameter."}
+	} else {
+		db := setupDB()
+
+		printMessage("Inserting movie into DB")
+
+		fmt.Println("Inserting new movie with ID: " + movieID + " and name: " + movieName)
+
+		var lastInsertID int
+		err := db.QueryRow("INSERT INTO movies(movieID, movieName) VALUES($1, $2) returning id;", movieID, movieName).Scan(&lastInsertID)
+
+		// check errors
+		checkErr(err)
+		db.Close()
+		response = JsonResponse{Type: "success", Message: "The movie has been inserted successfully!"}
+	}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+// Delete a movie
+
+// response and request handlers
+func DeleteMovie(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	movieID := params["movieid"]
+
+	var response = JsonResponse{}
+
+	if movieID == "" {
+		response = JsonResponse{Type: "error", Message: "You are missing movieID parameter."}
+	} else {
+		db := setupDB()
+
+		printMessage("Deleting movie from DB")
+
+		_, err := db.Exec("DELETE FROM movies where movieID = $1", movieID)
+
+		// check errors
+		checkErr(err)
+		db.Close()
+		response = JsonResponse{Type: "success", Message: "The movie has been deleted successfully!"}
+	}
+
+	json.NewEncoder(w).Encode(response)
+
+}
+
+// Delete all movies
+
+// response and request handlers
+func DeleteMovies(w http.ResponseWriter, r *http.Request) {
+	db := setupDB()
+
+	printMessage("Deleting all movies...")
+
+	_, err := db.Exec("DELETE FROM movies")
+
+	// check errors
+	checkErr(err)
+	db.Close()
+	printMessage("All movies have been deleted successfully!")
+
+	var response = JsonResponse{Type: "success", Message: "All movies have been deleted successfully!"}
+
+	json.NewEncoder(w).Encode(response)
+}
+
+// Go main function
+func main() {
+
+	// loading environment variables
+	/**
+	dialect := os.Getenv("DIALECT")
+	host := os.Getenv("HOST")
+	dpPort := os.Getenv("PORT")
+	user := os.Getenv("USER")
+	dbName := os.Getenv("NAME")
+	password := os.Getenv("PASSWORD")
+	*/
+
+	// Init the mux router
+	router := mux.NewRouter()
+	// Route handles & endpoints
+
+	// Get all movies
+	router.HandleFunc("/movies/", GetMovies).Methods("GET")
+
+	// Create a movie
+	router.HandleFunc("/create/movies/", CreateMovie).Methods("POST")
+
+	// Delete a specific movie by the movieID
+	router.HandleFunc("/movies/{movieid}", DeleteMovie).Methods("DELETE")
+
+	// Delete all movies
+	router.HandleFunc("/movies/", DeleteMovies).Methods("DELETE")
+
+	// serve the app
+	fmt.Println("Server at 8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
